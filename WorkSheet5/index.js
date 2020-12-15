cf46c5b8ce68ac3db46627bf5ab89ded91ca76d1
JacobLomas <73339199+JacobLomas@users.noreply.github.com>
2020-12-15 20:10:54 +0100

    Subiendo el ejercicio de RegExp

--- WorkSheet5/index.js
+++ WorkSheet5/index.js
@@ -1,20 +1,68 @@
 window.addEventListener("load", main);
 function main(){
-    validarNombreUsuario();    
-    
+    document.getElementById("nombre").addEventListener("focusout", validarNombreApellidos);
+    document.getElementById("apellidos").addEventListener("focusout", validarNombreApellidos);
+    document.getElementById("dni").addEventListener("focusout", validarDNI);
+    document.getElementById("telefono").addEventListener("focusout", validarTelefono);
+    document.getElementById("email").addEventListener("focusout", validarEmail);
+    document.getElementById("nombreUsuario").addEventListener("focusout", validarUsuario);
 };
 
 
-function validarNombreUsuario(){
-    var nombreUsuario=document.getElementById("nombreUsuario");
-    var val=new RegExp("^[a-zA-Z!#$%&/()=\s]+$");
-    nombreUsuario.addEventListener("keyup",function(e){
-        if(val.test(nombreUsuario.value) ){
-            e.target.style.backgroundColor="green";
-            e.target.style.opacity="50%"
-        }else{
-            e.target.style.backgroundColor="red";
-            e.target.style.opacity="50%"
-        }
-    })
+function validarNombreApellidos(e) {
+    let expresion = /\d/;
+    if (expresion.test(e.currentTarget.value)) {
+        e.target.style.backgroundColor="green";
+        e.target.style.opacity="50%"
+    } else {
+        e.currentTarget.setCustomValidity("");
+        e.target.style.backgroundColor="red";
+        e.target.style.opacity="50%"
+    }
+}
+
+function validarDNI(e){
+    let expresion = /^\d{8}[A-Za-z]$/;
+    if (expresion.test(e.currentTarget.value)) {
+        e.target.style.backgroundColor="green";
+        e.target.style.opacity="50%"
+    } else {
+        e.currentTarget.setCustomValidity("");
+        e.target.style.backgroundColor="red";
+        e.target.style.opacity="50%"
+    }
+}
+
+function validarTelefono(e){
+    let expresion = /^[6-9]\d{8}$/;
+    if (expresion.test(e.currentTarget.value)) {
+        e.target.style.backgroundColor="green";
+        e.target.style.opacity="50%"
+    } else {
+        e.currentTarget.setCustomValidity("");
+        e.target.style.backgroundColor="red";
+        e.target.style.opacity="50%"
+    }
+}
+function validarEmail(e){
+    let expresion = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (expresion.test(e.currentTarget.value)) {
+        e.target.style.backgroundColor="green";
+        e.target.style.opacity="50%"
+    } else {
+        e.currentTarget.setCustomValidity("");
+        e.target.style.backgroundColor="red";
+        e.target.style.opacity="50%"
+    }
+}
+function validarUsuario(e){
+    let expresion = /^(?=.*[A-Za-zÑñ])(?=.*\d)(?=.*[,;.:'"«»()[\]{}¿?¡!-])[A-Za-zÑñ\d,;.:'"«»()[\]{}¿?¡!\-@^_+|~`<>/$%*#&]{8,}$/;
+    if (expresion.test(e.currentTarget.value)) {
+        e.target.style.backgroundColor="green";
+        e.target.style.opacity="50%"
+    } else {
+        e.currentTarget.setCustomValidity("");
+        e.target.style.backgroundColor="red";
+        e.target.style.opacity="50%"
+    }
 }
